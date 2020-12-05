f274e1d6ac019d3e9b5dc94ceef709d8fb5cbef5
nirmash <nirmsk@hotmail.com>
2020-12-04 16:18:16 -0800

    try this

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { getCurrentUser } from '../utils/auth'
 
-const Home = () => <div>
+const Home = () =>{ 
+const user = getCurrentUser()
+return(<div>
   <h1>Home</h1>
-	<p>Hello my name is nir</p>
+	<p>Hello {user.username}</p>
 	<p>You are now logged in! <Link to="/app/profile">View profile</Link></p>
 	<p>Now go build something great and deploy it using the <a href="https://console.amplify.aws">AWS Amplify Console</a></p>
-	</div>
+	</div>)
+}
 
 export default Home
